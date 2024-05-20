c57289b88e44c9b8746820fe5e51aec751290e6c
mikylem2710 <49006053+mikylem2710@users.noreply.github.com>
2024-05-20 23:43:27 +0200

    changes to accomadate the requirements of question 3

--- src/main/java/sdp/prac2/App.java
+++ src/main/java/sdp/prac2/App.java
@@ -1,4 +1,3 @@
-//number 2 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Document;
@@ -6,8 +5,9 @@ import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import java.io.File;
 import java.util.Scanner;
+import org.json.JSONObject;
 
-public class XMLReader {
+public class App{
     public static void main(String[] args) {
         try {
             // Specify the path to your XML file
@@ -38,28 +38,38 @@ public class XMLReader {
             // Split the input into an array of field names
             String[] fieldNames = input.split(",");
 
-            // Iterate through each  element
+            // Iterate through each element
             for (int i = 0; i < recordList.getLength(); i++) {
                 Element record = (Element) recordList.item(i);
 
-                // Print out the selected field values
+                // Create a JSON object to hold selected fields for this record
+                JSONObject jsonObject = new JSONObject();
+
+                // Add selected fields to the JSON object
                 for (String fieldName : fieldNames) {
-                    NodeList fields = record.getElementsByTagName(fieldName.trim());
-                    if (fields.getLength() > 0) {
-                        System.out.println(fieldName.trim() + ": " + fields.item(0).getTextContent());
-                    } else {
-                        System.out.println(fieldName.trim() + ": Not found");
-                    }
+                    String fieldValue = getFieldValue(record, fieldName.trim());
+                    jsonObject.put(fieldName.trim(), fieldValue);
                 }
 
-                
+                // Print the JSON object for this record
+                System.out.println(jsonObject.toString());
+
                 System.out.println("-------------------------");
             }
 
-            
             scanner.close();
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    // Helper method to get the value of a field from a record element
+    private static String getFieldValue(Element record, String fieldName) {
+        NodeList fields = record.getElementsByTagName(fieldName);
+        if (fields.getLength() > 0) {
+            return fields.item(0).getTextContent();
+        } else {
+            return "Not found";
         }
     }
 }
